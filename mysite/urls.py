4f61aeb84c16314f2c961481eb73da1c345d7d13
deuter431 <lxmania7@gmail.com>
2019-03-06 20:57:15 +0900

    로그인 및 댓글 기능 구현 완료

--- mysite/urls.py
+++ mysite/urls.py
@@ -13,10 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# from django.urls import path, include
 from django.contrib import admin
-from django.urls import path, include
+from django.conf.urls import include, url
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('blog.urls')),
+    url(r'^admin/', admin.site.urls),
+    url(r'^accounts/login/$', auth_views.LoginView.as_view(), name='login'),
+    url(r'^accounts/logout/$', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
+    url(r'', include('blog.urls')),
 ]
